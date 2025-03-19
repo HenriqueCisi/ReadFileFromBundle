ea5758f98f7dacb43be04f802e3f53123b4c7f77
Henrique Cisi <henrique.cisi@accenture.com>
2025-03-19 16:33:47 -0300

    implements readFileFromBundle

--- ReadFileFromBundle/ViewController.swift
+++ ReadFileFromBundle/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        readFileFromBundle()
     }
-
-
+    
+    func readFileFromBundle() {
+        guard let filePath = getFilePath() else {
+            print("File path is nil. File processing failed.")
+            return
+        }
+        
+        if let fileContent = getFileContent(from: filePath) {
+            print("File content: \n\(fileContent)")
+        } else {
+            print("Error reading file content. File processing failed.")
+        }
+    }
+    
+    
+    func getFilePath() -> String? {
+        guard let filePath = Bundle.main.path(forResource: "json-placeholder", ofType: "txt") else {
+            print("File not found in bundle.")
+            return nil
+        }
+        return filePath
+    }
+    
+    func getFileContent(from filePath: String) -> String? {
+        do {
+            let fileContent = try String(contentsOfFile: filePath, encoding: .utf8)
+            return fileContent
+        } catch {
+            print("Error reading file: \(error)")
+            return nil
+        }
+    }
 }
-
