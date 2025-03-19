f6e068ef15de0f61e148ec660f5c1c2d4f6f5674
Henrique Cisi <henrique.cisi@accenture.com>
2025-03-19 16:28:30 -0300

    Initial Commit

---
+++ ReadFileFromBundle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReadFileFromBundle
+//
+//  Created by henrique.cisi on 19/03/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
